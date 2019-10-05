faefadb5c3cd4f98f6bbb263ad2ef67e5fa86271
Jorge Kamezawa <jor_kamezawa@hotmail.com>
2019-10-05 15:43:21 -0300

    Jorge TEstando

--- src/main/java/br/com/alura/forum/controller/HelloController.java
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 	@RequestMapping("/")
 	@ResponseBody
 	public String hello() {
-		return "Hello World - Agora estou no GitHub";
+		return "Hello World - Agora estou no GitHub 2";
 	}
 }
